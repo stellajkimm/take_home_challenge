fdca2ea95d46e144b8ba206b08c9f7abd59c3f23
Stella Kim <stellajkimm@gmail.com>
2014-10-17 12:07:13 -0400

    changed database to postgresql

--- take_home_challenge/settings.py
+++ take_home_challenge/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'take_home_challenge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'stellajkimm',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
