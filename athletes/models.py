79247dd8318fafc9065d4f9913f97e22dd118309
Stella Kim <stellajkimm@gmail.com>
2014-10-17 19:39:06 -0400

    added __str__

--- athletes/models.py
+++ athletes/models.py
@@ -3,17 +3,29 @@ from django.db import models
 class Sport(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class League(models.Model):
     sport = models.ForeignKey(Sport)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Division(models.Model):
     league = models.ForeignKey(League)
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Team(models.Model):
     division = models.ForeignKey(Division)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Athlete(models.Model):
     team = models.ForeignKey(Team)
@@ -22,3 +34,6 @@ class Athlete(models.Model):
     position = models.CharField(max_length=200)
     status = models.CharField(max_length=200)
     number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
