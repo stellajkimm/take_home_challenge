931f2fe8f1c2ccaff5adc1471739834e4a531e10
Stella Kim <stellajkimm@gmail.com>
2014-10-17 14:13:05 -0400

    added athlete model

--- athletes/models.py
+++ athletes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Athlete(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    sport = models.CharField(max_length=200)
+    league = models.CharField(max_length=200)
+    team = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    picture = models.URLField()
