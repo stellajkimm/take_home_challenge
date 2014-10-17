434eb8d5b9cdd18acbeb93bd8f65e3eec79de892
Stella Kim <stellajkimm@gmail.com>
2014-10-17 19:07:42 -0400

    added more models

--- athletes/models.py
+++ athletes/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 
+class Sport(models.Model):
+    name = models.CharField(max_length=200)
+
+class League(models.Model):
+    sport = models.ForeignKey(Sport)
+    name = models.CharField(max_length=200)
+
+class Division(models.Model):
+    league = models.ForeignKey(League)
+    name = models.CharField(max_length=200)
+
+class Team(models.Model):
+    division = models.ForeignKey(Division)
+    name = models.CharField(max_length=200)
+
 class Athlete(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
