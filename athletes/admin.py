9f17ee8a04b7515cf75f9e37bfaa88aac2d0fa08
Stella Kim <stellajkimm@gmail.com>
2014-10-17 21:45:04 -0400

    added search field to admin

--- athletes/admin.py
+++ athletes/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from athletes.models import Sport, League, Division, Team, Athlete
 
 class AthleteAdmin(admin.ModelAdmin):
-	list_display = ('first_name', 'last_name', 'team', 'position', 'number', 'status')
+	list_display = ('__str__', 'team', 'position', 'number', 'status')
+	search_fields = ('first_name', 'last_name')
 
 class LeagueAdmin(admin.ModelAdmin):
 	list_display = ('name', 'sport')
