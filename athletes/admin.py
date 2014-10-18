28973a9e564bbe44ba79b81fc2c6a48394426f50
Stella Kim <stellajkimm@gmail.com>
2014-10-17 21:26:43 -0400

    added admin stuff again

--- athletes/admin.py
+++ athletes/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from athletes.models import Sport, League, Division, Team, Athlete
+
+admin.site.register(Sport)
+admin.site.register(League)
+admin.site.register(Division)
+admin.site.register(Team)
+admin.site.register(Athlete)
