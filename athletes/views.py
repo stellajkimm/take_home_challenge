553208602f188a48d13522fff7e7a03bab385c6f
Stella Kim <stellajkimm@gmail.com>
2014-10-17 22:13:46 -0400

    first view works

--- athletes/views.py
+++ athletes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+from athletes.models import Sport, League, Division, Team, Athlete
+
+def index(request):
+	return HttpResponse('The Main Page')
