6b5e9b8d09967849c73c49ac206bac69f6ae5aac
Stella Kim <stellajkimm@gmail.com>
2014-10-17 22:27:53 -0400

    added index template

--- athletes/views.py
+++ athletes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.template import loader, Context
 from django.core.urlresolvers import reverse
 from django.views import generic
 
 from athletes.models import Sport, League, Division, Team, Athlete
 
 def index(request):
-	return HttpResponse('The Main Page')
+	sports = Sport.objects.all()
+	template = loader.get_template('athletes/index.html')
+	context = Context({'sports': sports})
+	response = template.render(context)
+	return HttpResponse(response)
