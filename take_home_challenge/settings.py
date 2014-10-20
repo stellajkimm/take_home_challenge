657ae4e07439e43c1cf5d61ac383ade4f4bab71c
Stella Kim <stellajkimm@gmail.com>
2014-10-20 18:07:48 -0400

    settings.py

--- take_home_challenge/settings.py
+++ take_home_challenge/settings.py
@@ -88,12 +88,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
+# )
 
 # Template Settings
 
