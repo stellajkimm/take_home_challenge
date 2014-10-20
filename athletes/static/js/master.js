a1a0baadb2461620186d1f7eead2e8f168e9a6c1
Stella Kim <stellajkimm@gmail.com>
2014-10-20 00:43:57 -0400

    search bar works

---
+++ athletes/static/js/master.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+	  $('#filter').keyup(function () {
+
+	      var rex = new RegExp($(this).val(), 'i');
+	      $('.searchable tr').hide();
+	      $('.searchable tr').filter(function () {
+	          return rex.test($(this).text());
+	      }).show();
+
+	  })
+
+});
