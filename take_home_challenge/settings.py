a59d0edccf0a36c76062387b8235eff75778fa86
Stella Kim <stellajkimm@gmail.com>
2014-10-20 23:46:18 -0400

    debug = false

--- take_home_challenge/settings.py
+++ take_home_challenge/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'p+sxv+)96wpnzstex-vk+w^e_3!jdemiilr(v-e75l20ts1%b^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
