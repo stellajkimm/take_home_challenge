f298cb4ca7c35a19e912c6c4aaea32713a3c244f
Stella Kim <stellajkimm@gmail.com>
2014-10-20 21:27:12 -0400

    database settings

--- take_home_challenge/settings.py
+++ take_home_challenge/settings.py
@@ -60,16 +60,16 @@ WSGI_APPLICATION = 'take_home_challenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'thuziodb',
-        'USER': 'stellajkimm',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'thuziodb',
+#         'USER': 'stellajkimm',
+#         'PASSWORD': 'password',
+#         'HOST': '',
+#         'PORT': '5432',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -99,8 +99,8 @@ STATICFILES_DIRS = (
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
